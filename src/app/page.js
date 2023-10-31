e25deb0c81cf8c6d51b56e0b7701c25f4e600441
Vadoo <example@example.com>
2023-10-31 18:37:57 +0330

    Added dashboard code to Dashboard.js file  Styled the dashboard using raw CSS  Added sidebar to Dashboard.js file Styled the sidebar using raw CSS

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
-
+import Sidebar from "../../components/Sidebar/sidebar";
+import Dashboard from "../../pages/index/index.jsx";
 
 export default function Home() {
   return (
-  <></>
-  )
+    <div style={{ display: "flex" }}>
+      <Sidebar style={{ marginRight: "auto" }} />
+      <Dashboard style={{ marginLeft: "auto" }} />
+    </div>
+  );
 }
